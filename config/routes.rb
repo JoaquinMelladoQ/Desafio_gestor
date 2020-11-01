1665a36978f1cb889e0606fa6dba0c2cf7969d79
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-01 01:24:05 -0300

    added a view for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'projects/index'
   root 'projects#index'
   post 'projects/create'
+  get 'dashboard', to: 'projects#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
