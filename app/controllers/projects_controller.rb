1d881154baa30665165f6c11df85ed7fb24185c3
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-01 01:09:10 -0300

    fixed bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,15 @@ class ProjectsController < ApplicationController
     @project = Project.new 
     @states = Project.states.keys.to_a
   end
+
+  def create
+    @project = Project.create(project_params)
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :start_on, :end_on, :state)
+  end
+
 end
